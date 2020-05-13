38ca7ac79e91f9dd5091e85fc136184de1ac0a18
jhs <jhs451342@naver.com>
2020-05-14 00:35:46 +0900

    activity_complete

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import android.app.Activity
+import android.util.Log
+import androidx.annotation.IdRes
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.main_toolbar.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu;
+
+
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(main_layout_toolbar) // 툴바를 액티비티의 앱바로 지정
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_dehaze_black_24dp)// 드로어를 꺼낼 홈 버튼 활성화
         supportActionBar?.setDisplayShowTitleEnabled(false) // 툴바에 타이틀 안보이게
         main_navigationView.setNavigationItemSelectedListener(this) //navigation 리스너
+
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
@@ -28,24 +40,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return super.onOptionsItemSelected(item)
 }
 
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.account-> Toast.makeText(this,"account clicked",Toast.LENGTH_SHORT).show()
-            R.id.item4-> Toast.makeText(this,"item4 clicked",Toast.LENGTH_SHORT).show()
+            R.id.item1-> { val item1Intent = Intent(this, SubActivity::class.java)
+            startActivity(item1Intent)}
+            R.id.item2-> Toast.makeText(this,"item4 clicked",Toast.LENGTH_SHORT).show()
             R.id.item3-> Toast.makeText(this,"item3 clicked",Toast.LENGTH_SHORT).show()
         }
         return false
     }
 
-    override fun onBackPressed() { //뒤로가기 처리
-        if(main_layout.isDrawerOpen(GravityCompat.START)){
-            main_layout.closeDrawers()
-            // 테스트를 위해 뒤로가기 버튼시 Toast 메시지
-            Toast.makeText(this,"back btn clicked",Toast.LENGTH_SHORT).show()
-        } else{
-            super.onBackPressed()
-        }
-    }
 
 }
